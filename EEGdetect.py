48370295885b3b357664fd43a33f23d46258d224
Eric McVoy Dodds <edodds828@gmail.com>
2015-07-28 13:59:09 -0700

    as used for first submission

--- EEGdetect.py
+++ EEGdetect.py
@@ -5,7 +5,7 @@ Created on Tue Jul 14 15:06:28 2015
 @author: Eric Dodds
 """
 
-#import pandas as pd
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,67 +15,82 @@ import time
 
 import preprocessing
 
-time.clock() 
-
-features, labels, nevents, ntrtimes = preprocessing.preprocess(subject = 1)
-
-# separate some data for cross-validation
-features_train, features_cv, labels_train, labels_cv = cross_validation.train_test_split(
-    features, labels, test_size = 0.3)
+def crossvalidation(subject=1):
+    time.clock() 
     
-preptime = time.clock()
-print ("Preprocessing took " + str(preptime) + " seconds.")
-
-# train classifiers. Note we can't use just one classifier object 
-# because some events overlap so we want to be able to predict combinations of classes
-classifiers = [LogisticRegression(C=1) for event in range(nevents)]
-for event in range(nevents):
-    classifiers[event].fit(features_train, labels_train[:,event])
+    features, labels, nevents, ntrtimes, ica, FTtstep, _ = preprocessing.preprocess(subject = subject)
     
-traintime = time.clock() - preptime
-print ("Trained the classifiers in " + str(traintime) + " seconds.")
+    preptime = time.clock()
+    print ("Preprocessing took " + str(preptime) + " seconds.")    
     
-# naively score classifiers on training set
-trscores = np.zeros((nevents))
-for event in range(nevents):
-    trscores[event] = classifiers[event].score(features_train, labels[:,event])
-print ("Scores on training set in binned time: " + str(trscores) )
+    # separate some data for cross-validation
+    features_train, features_cv, labels_train, labels_cv = cross_validation.train_test_split(
+        features, labels, test_size = 0.3)
+    
+    # train classifiers. Note we can't use just one classifier object 
+    # because some events overlap so we want to be able to predict combinations of classes
+    classifiers = [LogisticRegression(C=1) for event in range(nevents)]
+    for event in range(nevents):
+        classifiers[event].fit(features_train, labels_train[:,event])
+        
+    traintime = time.clock() - preptime
+    print ("Trained the classifiers in " + str(traintime) + " seconds.")
+        
+    # naively score classifiers on training set
+    trscores = np.zeros((nevents))
+    for event in range(nevents):
+        trscores[event] = classifiers[event].score(features_train, labels_train[:,event])
+    print ("Scores on training set in binned time: " + str(trscores) )
+    
+    # naively score classifiers on CV set
+    testscores = np.zeros((nevents))
+    for event in range(nevents):
+        testscores[event] = classifiers[event].score(features_cv, labels_cv[:,event])
+    print ("Scores on CV set in binned time: " + str(testscores) )
+    
+    
+    # generate ROC curves for CV set in binned time
+    predlabels_cv = np.transpose([classifiers[e].predict_proba(features_cv)[:,1] for e in range(nevents)])
+    trues_cv = predlabels_cv*labels_cv 
+    falses_cv = predlabels_cv*(1 - labels_cv)
+    thresholds = np.arange(0,1,.001)
+    falserates = [np.sum(falses_cv > th, axis=0)/np.sum(1-labels_cv,axis=0) for th in thresholds]
+    truerates = [np.sum(trues_cv > th, axis=0)/np.sum(labels_cv,axis=0) for th in thresholds]
+    plt.plot(falserates, truerates)
+    rocscores = np.abs(np.trapz(truerates, falserates, axis=0))
+    print("Areas under ROC curves:")
+    print (rocscores)
+    print ("Average ROC score:" + str(np.mean(rocscores)))
+    return np.mean(rocscores)
 
-# naively score classifiers on CV set
-testscores = np.zeros((nevents))
-for event in range(nevents):
-    testscores[event] = classifiers[event].score(features_cv, labels_cv[:,event])
-print ("Scores on CV set in binned time: " + str(testscores) )
+def do_all(subfile = "EEGbears.csv"):
+    cols = ['HandStart','FirstDigitTouch', 'BothStartLoadPhase','LiftOff','Replace','BothReleased']
+    ids_tot = []
+    pred_tot = []
+    for subject in range(1,9):
+        features_train, labels_train, nevents, ntrtimes, ica, FTtstep, _ = preprocessing.preprocess(subject = subject)
+        
+        # train classifiers. Note we can't use just one classifier object 
+        # because some events overlap so we want to be able to predict combinations of classes
+        classifiers = [LogisticRegression(C=1) for event in range(nevents)]
+        for event in range(nevents):
+            classifiers[event].fit(features_train, labels_train[:,event])
 
+        # read and prepare test data
+        features_test, _, _, ntesttimes, _, _, ids = preprocessing.preprocess(subject = subject,
+                                                                             train = False,
+                                                                             ica = ica)
+        ids_tot.append(ids)
+        # get predictions for individual time steps
+        ntimebins = features_test.shape[0]
+        predlabels = np.zeros((ntimebins,nevents))
+        for event in range(nevents):
+            predlabels[:,event] = classifiers[event].predict_proba(features_test)[:,1]
+        predevents = preprocessing.labels_to_events(predlabels, FTtstep, ntesttimes)
+        pred_tot.append(predevents)
+    # create pandas object for sbmission, write to file
+    submission = pd.DataFrame(index=np.concatenate(ids_tot),
+                          columns=cols,
+                          data=np.concatenate(pred_tot))                             
+    submission.to_csv(subfile, index_label='id', float_format='%.3f')
 
-# generate ROC curves for CV set in binned time
-predlabels_cv = np.transpose([classifiers[e].predict_proba(features_cv)[:,1] for e in range(nevents)])
-trues_cv = predlabels_cv*labels_cv 
-falses_cv = predlabels_cv*(1 - labels_cv)
-thresholds = np.arange(0,1,.001)
-falserates = [np.sum(falses_cv > th, axis=0)/np.sum(1-labels_cv,axis=0) for th in thresholds]
-truerates = [np.sum(trues_cv > th, axis=0)/np.sum(labels_cv,axis=0) for th in thresholds]
-plt.plot(falserates, truerates)
-rocscores = np.abs(np.trapz(truerates, falserates, axis=0))
-print("Areas under ROC curves:")
-print (rocscores)
-print ("Average ROC score:" + str(np.mean(rocscores)))
-
-# read and prepare test data
-#testseries = [9,10]
-#testfiles = ["../../Data/test/subj{0}_series{1}_data.csv".format(subject, s) for s in testseries]
-#rawtestdata = prepare(datafiles, read_events=False)
-#testfeatures, _ = get_features(ica, rawtestdata)
-#ntesttimebins = testfeatures.shape[0]
-
-# get predictions and errors for individual time steps
-#ntimes = rawdata.n_times
-#predlabels = np.zeros((ntimebins,nevents))
-#for event in range(nevents):
-#    predlabels[:,event] = classifiers[event].predict_proba(features)[:,1]
-#predevents = labels_to_events(predlabels, FTtstep, ntimes)
-#real_errors = np.sum(np.abs(events - predevents),0)/ntimes
-#print (real_errors)
-
-
-# TODO: write code to predict events in test data and put in submission format
--- preprocessing.py
+++ preprocessing.py
@@ -29,7 +29,7 @@ def prepare(datafiles, read_events = True):
         events = np.concatenate([pd.read_csv(f).values[:,1:] for f in eventfiles])
         return rawdata, events
     else:
-        return rawdata
+        return rawdata, None
 
 def get_logpsd(ica, rawdata):    
     """Given MNE ICA object already fit to data, and MNE RawArray, return numpy array
@@ -40,16 +40,20 @@ def get_logpsd(ica, rawdata):
     fourtrans = stft(sources.T, nsamples, tstep = FTtstep)
     return np.log(np.abs(fourtrans)**2), FTtstep
 
-def psd_to_features(logpsd, nbins = 7):
+def psd_to_features(logpsd, nbins = 7, usetwobins = False):
     """Given a numpy array of log PSD with shape (nchannels, nfreqs, ntimes), return matrix of feature vectors."""    
     nfreqs = logpsd.shape[1]
     ntimebins = logpsd.shape[2]
     freqsperbin = int(nfreqs/nbins)
     features = decimate(logpsd, freqsperbin, axis=1) # downsample in frequency dimension
-
-    # flatten into a vector for each time point
+    # flatten into a vector for each time point if not already, then make time first index
     features = np.array(features)
     features = features.reshape(-1, ntimebins).T
+    if usetwobins: # False by default since doesn't seem to help
+        # also use features from previous time bin. 
+        prevfeatures = np.zeros(features.shape[1])
+        prevfeatures = np.concatenate((prevfeatures[np.newaxis,:],features[:-1,:]),axis=0)
+        features = np.concatenate((features, prevfeatures), axis=1)
     return features, ntimebins 
 
 def get_features(ica, rawdata):
@@ -70,31 +74,43 @@ def events_to_labels(events, FTtstep, ntimebins):
 def labels_to_events(labels, FTtstep, ntimes):
     """Returns array of events corresponding to given labels; interpolation
     performed by just taking the value of the nearest bin-center."""
-    events = np.zeros((ntimes, labels.shape[-1]))
+    events = np.zeros((ntimes, labels.shape[1]))
     ntimebins = labels.shape[0]
     for timebin in range(ntimebins-1):
         events[FTtstep*(timebin+1):FTtstep*(timebin+2),:] = labels[timebin,:]
     return events   
     
-def preprocess(subject = 1):
+def preprocess(subject = 1, train = True, ica = None):
     """Preprocess data for given subject number. Returns feature matrix, label/target matrix,
-    the number of events (6), and the number of timepoints in the raw data."""
-    trseries = range(1,9)
-    datafiles = ["../../Data/train/subj{0}_series{1}_data.csv".format(subject, s) for s in trseries]
-    rawdata, events = prepare(datafiles)
-    ntrtimes = rawdata.n_times
-    nevents = events.shape[-1]
+    the number of events (6), and the number of timepoints in the raw data.
+    For test data, pass in pretrained ICA object. For training data, an ICA
+    object is created. Either way the ICA is returned."""
+    series = range(1,9) if train else range(9,11)
+    trtest = "train" if train else "test"
+    datafiles = ["../../Data/{0}/subj{1}_series{2}_data.csv".format(trtest, subject, s) for s in series]   
+    rawdata, events = prepare(datafiles, read_events = train)
+
+    ids = np.concatenate([np.array(pd.read_csv(fname)['id']) for fname in datafiles])
+    ntimes = rawdata.n_times
+    nevents = events.shape[-1] if train else None
     
-    ica = ICA(verbose=False)
-    ica.fit(rawdata)
+    if ica is None:    
+        ica = ICA(verbose=False)
+        ica.fit(rawdata)
+    else:
+        ica = ica
     
     # get features and labels in time bins associated with fourier transform
     logpsd, FTtstep = get_logpsd(ica, rawdata)
     features, ntimebins = psd_to_features(logpsd)
-    labels = events_to_labels(events, FTtstep, ntimebins)
+    
+    if train:
+        labels = events_to_labels(events, FTtstep, ntimebins)
+    else:
+        labels = None
     
     # feature scaling (remove mean and standard deviation)
     scaler = StandardScaler()
     features = scaler.fit_transform(features)
     
-    return features, labels, nevents, ntrtimes
+    return features, labels, nevents, ntimes, ica, FTtstep, ids
